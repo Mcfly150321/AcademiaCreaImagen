01aeefd99b67781c1db208d55864e92013cd3214
Mcfly150321 <20arturomaldonado@umes.edu.gt>
2026-01-19 15:43:23 -0600

    corigiendo ritas backend3

--- api/models.py
+++ api/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Float
 from sqlalchemy.orm import relationship
-from backend.database import Base
+from .database import Base
 
 class Student(Base):
     __tablename__ = "students"
